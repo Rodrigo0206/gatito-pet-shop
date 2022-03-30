7cebcafb232cc079edebe20b3293a9bbca825e30
unknown <rodrigo.domingues.87@hotmail.com>
2022-03-30 15:35:30 -0300

    icon e spash

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>hello word </Text>
+      <Text>teste </Text>
       <StatusBar style="auto" />
     </View>
   );
