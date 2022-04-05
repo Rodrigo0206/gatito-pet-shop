7db7742696c34f91370f4403bbcfcc82c043673f
unknown <rodrigo.domingues.87@hotmail.com>
2022-04-05 14:52:23 -0300

    index item

--- src/telas/Servicos/index.js
+++ src/telas/Servicos/index.js
@@ -1,37 +1,36 @@
 import React from 'react';
 
-import { SafeAreaView, StatusBar, Text, FlatList } from 'react-native';
+import { SafeAreaView, StatusBar, FlatList } from 'react-native';
 import Item from './Item';
 
 const servicos = [
-    {
-        id: 1,
-        nome: "Banho",
-        preco: 79.9,
-        descricao: "NÃO DE BANHO NO SEU GATO! Mas se você quiser nós damos."
-    },
-    {
-        id: 2,
-        nome: "Vacina V4",
-        preco: 89.9,
-        descricao: "Uma dose da vacina V4.Seu gato precisa de duas."   
-    },
-    {
-        id: 3,
-        nome: "Vacina Antirrabica",
-        preco: 99.9,
-        descricao: "Uma dose da vacina antirrabica. Seu gato precisa de uma por ano."   
-    }
+  {
+    id: 1,
+    nome: "Banho",
+    preco: 79.9,
+    descricao: "NÃO DE BANHO NO SEU GATO! Mas se precisar nós damos."
+  },
+  {
+    id: 2,
+    nome: "Vacina V4",
+    preco: 89.9,
+    descricao: "Uma dose da vacina V4. Seu gato precisa de duas."
+  },
+  {
+    id: 3,
+    nome: "Vacina Antirrábica",
+    preco: 99.9,
+    descricao: "Uma dose da vacina antirrábica. Seu gato precisa de uma por ano."
+  }
 ];
 
-export default function Servicos(){
-    return<SafeAreaView>
-        <StatusBar/>
-        <Text>Servicos!</Text>
-        <FlatList
-            data={servicos}
-            renderItem={({item}) => <Item {...item} />}
-            keyExtractor={({id}) => String(id)}
-        />
-    </SafeAreaView>
+export default function Servicos() {
+  return <SafeAreaView>
+    <StatusBar />
+    <FlatList
+      data={servicos}
+      renderItem={({item}) => <Item {...item}  />}
+      keyExtractor={({id}) => String(id)}
+    />
+  </SafeAreaView>
 }
